96c5c8f62292277b96e569d1f4e622778200bd50
ssanapureddy <sravani.sanapureddy@gmail.com>
2019-12-23 16:56:38 -0800

    3rd commit

---
+++ discovery-service/src/main/java/com/edureka/spring1/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.edureka.spring1.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+@EnableEurekaServer
+
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
