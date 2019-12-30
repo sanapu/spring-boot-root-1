3c43a26d5455bebdc56f0af4e70b49f2db116014
ssanapureddy <sravani.sanapureddy@gmail.com>
2019-12-30 11:45:00 -0800

    6th commit

---
+++ edge-service/src/test/java/com/edureka/spring1/training/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.spring1.training.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
